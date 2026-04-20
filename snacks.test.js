8a3b6cae4f68c653cf5447dd83dd994ffaf1b143
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-04-20 17:23:44 +0200

    challenge snack: group snacks by their themes

--- snacks.test.js
+++ snacks.test.js
@@ -6,45 +6,50 @@ const {
   findPostById
 } = require("./snacks");
 
-// Snack 1
-test('La funzione "getInitials" restituisce le iniziali di un nome completo', () => { 
-  expect(getInitials("Mario Rossi")).toBe("MR");
+describe("Manipolazione di stringhe", () => {
+  // Snack 1
+  test('La funzione "getInitials" restituisce le iniziali di un nome completo', () => { 
+    expect(getInitials("Mario Rossi")).toBe("MR");
+  });
+
+  // Snack 2
+  test('La funzione "createSlug" restituisce una stringa in lowercase', () => { 
+    expect(createSlug("JAVAsCript")).toBe("javascript");
+    expect(createSlug("JAVAsCript è complesso")).toBe("javascript-è-complesso");
+  });
+  
+  // Snack 4
+  test('La funzione "createSlug" sostituisce gli spazi con "-"', () => { 
+    expect(createSlug("Questo è un test")).toBe("questo-è-un-test");
+  });
+
+  // Snack 5
+  test('La funzione "isPalindrome" verifica se una stringa è un palindromo', () => { 
+    expect(isPalindrome("anna")).toBeTruthy();
+  });
+
+  // Snack 6
+  test('La funzione "createSlug" lancia errore se il titolo è vuoto o non valido', () => { 
+    expect(() => createSlug("")).toThrow();
+    expect(() => createSlug(null)).toThrow();
+  })
 });
 
-// Snack 2
-test('La funzione "createSlug" restituisce una stringa in lowercase', () => { 
-  expect(createSlug("JAVAsCript")).toBe("javascript");
-  expect(createSlug("JAVAsCript è complesso")).toBe("javascript-è-complesso");
+describe("Manipolazione di array", () => {
+  // Snack 3
+  test('La funzione "average" calcola la media aritmetica di un array di numeri', () => { 
+    expect(average([1, 2, 3, 4, 5])).toBe(3);
+  });
+  
+  // Snack 7
+  const posts = [
+    { id: 1, title: "Post 1", slug: "post-1" },
+    { id: 2, title: "Post 2", slug: "post-2" },
+    { id: 3, title: "Post 3", slug: "post-3" }
+  ];
+  
+  test('La funzione "findPostById" restituisce il post corretto dato l\'array di post e l\'id', () => { 
+    expect(findPostById(posts, 1)).toEqual({ id: 1, title: "Post 1", slug: "post-1" });
+  });
 });
 
-// Snack 3
-test('La funzione "average" calcola la media aritmetica di un array di numeri', () => { 
-  expect(average([1, 2, 3, 4, 5])).toBe(3);
-});
-
-// Snack 4
-test('La funzione "createSlug" sostituisce gli spazi con "-"', () => { 
-  expect(createSlug("Questo è un test")).toBe("questo-è-un-test");
-});
-
-// Snack 5
-test('La funzione "isPalindrome" verifica se una stringa è un palindromo', () => { 
-  expect(isPalindrome("anna")).toBeTruthy();
-});
-
-// Snack 6
-test('La funzione "createSlug" lancia errore se il titolo è vuoto o non valido', () => { 
-  expect(() => createSlug("")).toThrow();
-  expect(() => createSlug(null)).toThrow();
-})
-
-// Snack 7
-const posts = [
-  { id: 1, title: "Post 1", slug: "post-1" },
-  { id: 2, title: "Post 2", slug: "post-2" },
-  { id: 3, title: "Post 3", slug: "post-3" }
-];
-
-test('La funzione "findPostById" restituisce il post corretto dato l\'array di post e l\'id', () => { 
-  expect(findPostById(posts, 1)).toEqual({ id: 1, title: "Post 1", slug: "post-1" });
-})
