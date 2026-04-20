6f664fbb282ad92b25e978fd8238fea34f7b9248
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-04-20 09:16:04 +0200

    snack 2: create testing function to transform to lowercase a string

--- snacks.js
+++ snacks.js
@@ -1,9 +1,13 @@
-//Snack 1
+// Snack 1
 const getInitials = fullname => {
   const initials = fullname.split(" ");
   return initials[0].slice(0, 1) + initials[1].slice(0, 1);
 }
 
+// Snack 2
+const createSlug = string => string.toLowerCase();
+
 module.exports = {
-  getInitials
+  getInitials,
+  createSlug
 }
--- snacks.test.js
+++ snacks.test.js
@@ -1,6 +1,14 @@
-const { getInitials } = require("./snacks");
+const { 
+  getInitials,
+  createSlug
+} = require("./snacks");
 
 // Snack 1
 test('La funzione "getInitials" restituisce le iniziali di un nome completo', () => { 
   expect(getInitials("Mario Rossi")).toBe("MR");
-})
+});
+
+// Snack 2
+test('La funzione "createSlug" restituisce una stringa in lowercase', () => { 
+  expect(createSlug("JAVAsCript")).toBe("javascript");
+});
